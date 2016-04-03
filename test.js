b4917b900060ffef6e64fd2f386a5281aa6558f1
Tanya Goncalves <goncat@mcmaster.ca>
2016-04-03 12:52:23 -0400

    New changes to test.js to allow playback rate changes and a hanning-ish envelope design

--- test.js
+++ test.js
@@ -18,51 +18,51 @@ function getData() {
 } 
 
 getData();
-// console.log(audioBuffer.getChannelData(0));
- 
 
 var Grain = function(index) {
 	this.index = index;
-    // this.sine = ac.createOscillator();
-    // this.sine.type = 'sine';
-    // this.sine.frequency.value = 440;
-	// this.source = ac.createBufferSource();
-    // this.source.buffer = audioBuffer;
     this.amp = 10;
+    this.dur = 1;
 	this.gain = ac.createGain();
-	// this.source.connect(this.gain);
 	this.gain.connect(ac.destination);
     this.gain.gain.setValueAtTime(0,ac.currentTime);
-	// this.source.start(0);
 	this.playing = false;
 }
 
 // Set the audiobuffersourcenode -> starting position for grain -> duration change (arg) -> env design 
 
 var synthBank = new Array();
+
 for(var n=0;n<10;n++) {
-	synthBank[n] = new Grain(n);
+    synthBank[n] = new Grain(n);
 }
 
-Grain.prototype.play = function() {
+
+function hanning(index,x) {
+    var win = 0.5 * (1 - Math.cos((2*(Math.PI)*(index))/(x-1)));
+    return win;
+}
+
+
+Grain.prototype.play = function(amp,dur,rate,start,win) {
 	if(this.playing == false) {
 		var now = ac.currentTime;
+        var x = 32;
 		this.gain.gain.setValueAtTime(0,now);
-        
-        // rewind audio source buffer node to the right place or else you will not hear anything
-        // loopstart to control centPos
-        
-        // 1. make audio buffer source node
-        // 2. connect audio buffer source node to the gain object stored previously
-        // 3. make the audio source node buffer go
         
         this.source = ac.createBufferSource();
         this.source.buffer = audioBuffer;
         this.source.connect(this.gain);
-        this.source.start(now);
         
-		this.gain.gain.linearRampToValueAtTime(this.amp,now+0.095);
-		this.gain.gain.linearRampToValueAtTime(0,now+1);
+        this.source.start(now,start,win);
+        
+        for(n=0;n<=x;n++){
+            this.gain.gain.linearRampToValueAtTime(amp*hanning(n,x),now+this.dur/x);
+            console.log(hanning(n,x));
+        }
+        
+        this.source.playbackRate.value = rate;
+        
 		this.playing = true;
 	}
 	var index = this.index;
@@ -70,6 +70,7 @@ Grain.prototype.play = function() {
 		synthBank[index].playing = false;
 	},1000);
 }
+
 
 Grain.prototype.dealloc = function() {
 	this.source.stop();
@@ -81,20 +82,17 @@ Grain.prototype.isNotPlaying = function () {
 	return (this.playing == false);
 }
 
-// somewhere else where we are using the class/object
-// when you make the grain its starting to read from the beginning of the sample... play method needs to rewind the audio source buffer node to the beginning of the grain...
-
-function playASynthFromTheBank() {
+function playASynthFromTheBank(amp,dur,rate) {
 var n;
 for(n=0;n<10;n++) {
 	if(synthBank[n].isNotPlaying())break;
 }
 if(n<10) { // we found one that is not playing
-	synthBank[n].play();
+	synthBank[n].play(amp,dur,rate);
 } else {
 	console.log("sorry too many notes playing right now");
 }
-}
+} 
 
 // playASynthFromTheBank();
 
@@ -112,3 +110,5 @@ timeOutResponder = function() {
 	playASynthFromTheBank(grainAmplitude);
 	setTimeout(timeOutResponder,grainPeriod);
 }
+
+
